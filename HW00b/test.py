ee20eab72be3b20d7ad910b0a8c001ea4067f3be
Billyye4 <bye@stevens.edu>
2026-01-21 17:39:36 -0500

    adds HW00b

---
+++ HW00b/test.py
@@ -0,0 +1,19 @@
+import unittest
+from triangle import defineTriangle
+
+class TestTriangle(unittest.TestCase):
+
+    def testEquilateral(self):
+        self.assertEqual(defineTriangle(3, 3, 3), "Equilateral")
+
+    def testIsosceles(self):
+        self.assertEqual(defineTriangle(3, 3, 5), "Isosceles")
+
+    def testScalene(self):
+        self.assertEqual(defineTriangle(3, 4, 6), "Scalene")
+
+    def testRightTriangle(self):
+        self.assertEqual(defineTriangle(3, 4, 5), "Right Triangle")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ HW00b/triangle.py
@@ -0,0 +1,15 @@
+# Source code to determine the type of triangle based on side lengths
+
+def defineTriangle (a,b,c):
+    if (a == b and b == c):
+        return "Equilateral"
+    elif (a == b or b == c or a == c):
+        return "Isosceles"
+    elif (a**2 + b**2 == c**2) or (a**2 + c**2 == b**2) or (b**2 + c**2 == a**2):
+        return "Right Triangle"
+    elif (a != b and b != c and a != c):
+        return "Scalene"
+    elif (a <= 0 or b <= 0 or c <= 0):
+        return "Not a triangle"
+    else:
+        return "Not a triangle"
