b6a9a58e2cda4d46af91cc6cadadfbf95845515b
Billyye4 <bye@stevens.edu>
2026-02-16 20:52:47 -0500

    Fixes yml file location

--- HW03/HW03a/hw03a_test.py
+++ HW03/HW03a/hw03a_test.py
@@ -5,7 +5,7 @@ class TestGitHubInfo(unittest.TestCase):
 
     def testGetInfo(self):
         result = getInfo("Billyye4")
-        expected = ["Repo: aaa-refactor-tool-python- Number of commits: 5", "Repo: SSW-567 Number of commits: 7"]
+        expected = ["Repo: aaa-refactor-tool-python- Number of commits: 5", "Repo: SSW-567 Number of commits: 11"]
         self.assertEqual(result, expected)
 
     def testGetInfoInvalidUser(self):
