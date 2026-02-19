7411ffa3bfe6d73093421ddd241446fe1d8804ac
Billye4 <yebilly4@gmail.com>
2026-02-19 17:28:08 -0500

    Adds mocking to HW03a to isolate github API dependency

--- HW03/HW03a/hw03a_test.py
+++ HW03/HW03a/hw03a_test.py
@@ -1,14 +1,34 @@
 import unittest
+from unittest.mock import patch, Mock
 from hw03a import getInfo
 
 class TestGitHubInfo(unittest.TestCase):
 
-    def testGetInfo(self):
+    @patch('hw03a.requests.get')
+    def testGetInfo(self, mock_GetInfo):
+        # Mock the response for the user's repositories
+        mock_RepoResponse = Mock()
+        mock_RepoResponse.status_code = 200
+        mock_RepoResponse.json.return_value = [{'name': 'SSW-567'}]
+
+        # Mock the response for the commits in the repository
+        mock_CommitsResponse = Mock()
+        mock_CommitsResponse.status_code = 200
+        mock_CommitsResponse.json.return_value = [1] * 6  # Simulating 6 commits
+
+        # Set the side effects for the mock
+        mock_GetInfo.side_effect = [mock_RepoResponse, mock_CommitsResponse]
         result = getInfo("BillyYe1")
         expected = ['Repo: SSW-567 Number of commits: 6']
         self.assertEqual(result, expected)
 
-    def testGetInfoInvalidUser(self):
+    @patch('hw03a.requests.get')
+    def testGetInfoInvalidUser(self, mock_GetInfo):
+        mock_InvalidResponse = Mock()
+        mock_InvalidResponse.status_code = 404
+        mock_GetInfo.return_value = mock_InvalidResponse
+        #Side effect not required here since we are only testing one call to requests.get
+
         result = getInfo("InvalidUser!")
         expected = "Error: 404"
         self.assertEqual(result, expected)
