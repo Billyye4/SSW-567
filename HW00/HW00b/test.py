afb078ec15f1c3b916e672bf72847cf01ec64ebe
Billye4 <yebilly4@gmail.com>
2026-02-19 20:39:08 -0500

    Completed HW03c Static and Coverage Analysis

--- HW00/HW00b/test.py
+++ HW00/HW00b/test.py
@@ -1,22 +1,22 @@
 import unittest
-from triangle import defineTriangle
+from triangle import define_triangle
 
 class TestTriangle(unittest.TestCase):
 
     def testEquilateral(self):
-        self.assertEqual(defineTriangle(3, 3, 3), "Equilateral")
+        self.assertEqual(define_triangle(3, 3, 3), "Equilateral")
 
     def testIsosceles(self):
-        self.assertEqual(defineTriangle(3, 3, 5), "Isosceles")
+        self.assertEqual(define_triangle(3, 3, 5), "Isosceles")
 
     def testScalene(self):
-        self.assertEqual(defineTriangle(3, 4, 6), "Scalene")
+        self.assertEqual(define_triangle(3, 4, 6), "Scalene")
 
     def testRightTriangle(self):
-        self.assertEqual(defineTriangle(3, 4, 5), "Right Triangle")
+        self.assertEqual(define_triangle(3, 4, 5), "Right Triangle")
 
     def testNotTriangle(self):
-        self.assertEqual(defineTriangle(3, 0, 5), "Not a triangle")
+        self.assertEqual(define_triangle(3, 0, 5), "Not a triangle")
 
 if __name__ == '__main__':
     unittest.main()
--- HW00/HW00b/triangle.py
+++ HW00/HW00b/triangle.py
@@ -1,17 +1,17 @@
-# Source code to determine the type of triangle based on side lengths
+"""
+Source code to determine the type of triangle based on side lengths
+"""
 
-def defineTriangle (a,b,c):
+def define_triangle (a,b,c):
+    """Returns the type of triangle given three sides."""
     if (a <= 0 or b <= 0 or c <= 0):
-        return "Not a triangle"
-    elif (a <= 0 or b <= 0 or c <= 0):
         return "Not a triangle"
     if (a == b and b == c):
         return "Equilateral"
-    elif (a == b or b == c or a == c):
+    if (a == b or b == c or a == c):
         return "Isosceles"
-    elif (a**2 + b**2 == c**2) or (a**2 + c**2 == b**2) or (b**2 + c**2 == a**2):
+    if (a**2 + b**2 == c**2) or (a**2 + c**2 == b**2) or (b**2 + c**2 == a**2):
         return "Right Triangle"
-    elif (a != b and b != c and a != c):
+    if (a != b and b != c and a != c):
         return "Scalene"
-    else:
-        return "Not a triangle"
+    return "Not a triangle"
